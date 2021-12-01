196a35f3ab9b0071a1db30aa31546a08dd6031fe
codymart1986 <codymart1986@hotmail.com>
2021-11-30 16:26:15 -0800

    adjusted user schema and validation

--- models/user.js
+++ models/user.js
@@ -3,16 +3,20 @@ const Joi = require('joi')
 const { productSchema } = require('./products')
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, minLength: 5, maxLength: 50 },
+  email: { type: String, unique: true, required: true, minLength: 5, maxLength: 255 },
+  password: {type: String, required: true, maxLength: 1024, minLength: 5 },
   isGoldMember: { type: String, default: false },
   shoppingCart: { type: [productSchema], default: [] },
 })
 
 const User = mongoose.model('User', userSchema)
 
-const validateUser = (user) => {
+function validateUser = (user) => {
   const schema = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
   })
   return schema.validate(user)
 }
